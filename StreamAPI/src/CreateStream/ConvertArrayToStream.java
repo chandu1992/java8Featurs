d5a61b84013c506dc5a883795e5993520df5b14b
chandrashekharbhaupande <chpande55@gmail.com>
2025-05-23 11:51:15 +0530

    code chnages

---
+++ StreamAPI/src/CreateStream/ConvertArrayToStream.java
@@ -0,0 +1,15 @@
+package CreateStream;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ConvertArrayToStream {
+
+	public static void main(String[] args) {
+		
+		String[] countries = {"India","Pakistan","USA","AUS"};
+		
+		Stream<String> s = Arrays.stream(countries);
+		s.forEach(System.out::println);
+	}
+}
--- StreamAPI/src/FunctionalInterface/FunctionInterfaceDemo.java
+++ StreamAPI/src/FunctionalInterface/FunctionInterfaceDemo.java
@@ -13,5 +13,21 @@ public class FunctionInterfaceDemo {
 		Function<String,Integer> fun = name -> name.length();
 		
 		System.out.println("Length of String is : "+ fun.apply("Chandra"));
+		
+		
+		Function<Integer,Integer> f1 = num -> 2 * num;
+		Function<Integer, Integer> f2 = num -> num * num * num ;
+		
+//		Function<Integer,Integer> stringFun = f1.andThen(f2);
+//		System.out.println(stringFun.apply(4));
+		
+		System.out.println(f1.andThen(f2).apply(3)); //216
+		System.out.println(f2.andThen(f1).apply(3)); //54
+		System.out.println(f1.compose(f2).apply(3)); //54 ̑›
+		
+		Function<String,String> identityFun = Function.identity();
+		System.out.println(identityFun.apply("Chandu")); //chandu
+		
+		
 	}
 }
