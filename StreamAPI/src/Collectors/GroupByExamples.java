b917be0473ac384a19a5818279c7ee09f55ab9c4
chandrashekharbhaupande <chpande55@gmail.com>
2025-05-25 21:54:58 +0530

    stream Collectors example

--- StreamAPI/src/Collectors/GroupByExamples.java
+++ StreamAPI/src/Collectors/GroupByExamples.java
@@ -22,14 +22,29 @@ public class GroupByExamples {
 		
 		Map<String, List<Student>> data = students.stream().collect(Collectors.groupingBy(Student::getName));
 		System.out.println(data);
+		/** { Chandra=[Student [name=Chandra, city=New York, age=33], Student [name=Chandra, city=Sangamner, age=10]],
+		      Gauri=[Student [name=Gauri, city=London, age=27]], 
+		      Poorva=[Student [name=Poorva, city=Paris, age=30]], 
+		      Raj=[Student [name=Raj, city=Paris, age=19]], 
+		      Akash=[Student [name=Akash, city=New York, age=29]], 
+		      Om=[Student [name=Om, city=London, age=25]]} **/
 		
 		Map<String,Long> data2 = students.stream().collect(Collectors.groupingBy(Student::getName,Collectors.counting()));
 		System.out.println(data2);
+		// {Chandra=2, Gauri=1, Poorva=1, Raj=1, Akash=1, Om=1}
 		
+		Map<String, List<String>> namesByAge = students.stream()
+			    .collect(Collectors.groupingBy(
+			    		Student::getName,
+			        Collectors.mapping(Student::getCity, Collectors.toList())
+			    ));
+
+		System.out.println(namesByAge);
+		// {Chandra=[New York, Sangamner], Gauri=[London], Poorva=[Paris], Raj=[Paris], Akash=[New York], Om=[London]}
 		
 		
 
-
+		System.out.println(students.stream().collect(Collectors.counting())); // 7
 
 
 	}
--- StreamAPI/src/Collectors/MethodsOfCollector.java
+++ StreamAPI/src/Collectors/MethodsOfCollector.java
@@ -11,16 +11,16 @@ public class MethodsOfCollector {
 	public static void main(String[] args) {
 		
 		List<String> listconv = Stream.of("Apple","Banana","Mango").collect(Collectors.toList());
-		System.out.println(listconv);
+		System.out.println(listconv);  // [Apple, Banana, Mango]
 		
 		Set<String> set = Stream.of("Apple","Apple","Mango").collect(Collectors.toSet());
-		System.out.println(set);
+		System.out.println(set);   // [Apple, Mango]
 		
 		String s = Stream.of("A","B","C").collect(Collectors.joining());
-		System.out.println(s);
+		System.out.println(s);   // ABC
 		
 		String s1 = Stream.of("A","B","C").collect(Collectors.joining(",","[","]"));
-		System.out.println(s1);
+		System.out.println(s1);   // [A,B,C]
 	}
 
 }
