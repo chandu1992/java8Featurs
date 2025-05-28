e8cfd8a401bbfa615bcb4ae3cfe61b5948f12cf1
chandrashekharbhaupande <chpande55@gmail.com>
2025-05-28 22:45:32 +0530

    code

---
+++ StreamAPI/src/FindMaxMinSalary/PrintFirstTwoStudentHavingAge.java
@@ -0,0 +1,25 @@
+package FindMaxMinSalary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintFirstTwoStudentHavingAge {
+	
+	public static void main(String[] args) {
+	List<Employee> employees = new ArrayList<Employee>();
+		
+		employees.add(new Employee(101,"Chandra",520000.4));
+		employees.add(new Employee(102,"Akash",600000.44));
+		employees.add(new Employee(103,"Gauri",400000.43));
+		employees.add(new Employee(103,"Arjun",4200000.45));
+		employees.add(new Employee(103,"Sumit",200000.4));
+		employees.add(new Employee(103,"Atul",100000.4));
+		
+		employees.stream()
+		.filter(emp -> emp.getSalary()>400000)
+		.map(emp -> emp.getName())
+		.limit(2)
+		.forEach(emp -> System.out.println(emp));
+	}
+
+}
